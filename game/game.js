b76404d87651b75286cd1865ab91b7d096e631c9
a15osmla <osmarolandaverde@gmail.com>
2018-03-07 19:32:03 +0100

    Added a game loop

--- game/game.js
+++ game/game.js
@@ -1,18 +1,94 @@
-var ctx, width, height;
+var ctx, width, height, canvas;
+var Game = {};
+var Player = {};
+var keys = {};
 
-function start() {
-    console.log("start");
-    width = $(window).width();
-    height =  $(window).height();
-    var canvas = document.getElementById("gamecanvas");
-    
+Game.width = $(window).width();
+Game.height = $(window).height();
+Game.fps = 60;
+Game.maxFrameSkip = 10;
+Game.skipTicks = 1000 / Game.fps;
+
+
+/* -------------------------------------------------------------------------- */
+
+Game.initialize = function() {
+    canvas = document.getElementById("gamecanvas");
     if (canvas && canvas.getContext) {
         ctx = canvas.getContext("2d");
+        canvas.width = Game.width;
+        canvas.height = Game.height;
     }
-    
-    canvas.width = width;
-    canvas.height = height;
-    
+};
+
+/* -------------------------------------------------------------------------- */
+
+Game.update = function(tick) {
+    Game.tick = tick;
+}
+
+/* -------------------------------------------------------------------------- */
+
+Game.draw = function() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+    ctx.fill();
+};
+
+/* -------------------------------------------------------------------------- */
+Game.pause = function() {
+   this.paused = (this.paused) ? false : true;
+};
+
+
+Game.run = (function() {
+    var loops = 0;
+    var nextGameTick = (new Date).getTime();
+    var startTime = (new Date).getTime();
+    return function() {
+        loops = 0;
+        while (!Game.paused && (new Date).getTime() > nextGameTick && loops < Game.maxFrameSkip) {
+            Game.update(nextGameTick - startTime);
+            nextGameTick += Game.skipTicks;
+            loops++;
+        }
+        Game.draw();
+    };
+})();
+
+/* -------------------------------------------------------------------------- */
+
+(function() {
+    var onEachFrame;
+    if (window.requestAnimationFrame) {
+       onEachFrame = function(cb) {
+          var _cb = function() {
+                cb();
+             requestAnimationFrame(_cb);
+          };
+          _cb();
+       };
+    } else if (window.webkitRequestAnimationFrame) {
+       onEachFrame = function(cb) {
+          var _cb = function() {
+             cb();
+             webkitRequestAnimationFrame(_cb);
+          };
+          _cb();
+       };
+    } else if (window.mozRequestAnimationFrame) {
+        onEachFrame = function(cb) {
+            var _cb = function() {
+                cb();
+                mozRequestAnimationFrame(_cb);
+            };
+            _cb();
+        };
+    } else {
+        onEachFrame = function(cb) {
+            setInterval(cb, Game.skipTicks);
+        };
+    }
+
+    window.onEachFrame = onEachFrame;
+})();
