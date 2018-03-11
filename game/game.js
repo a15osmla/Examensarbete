2419b1b076bb6952f82b58768ed28e04338b3525
a15osmla <osmarolandaverde@gmail.com>
2018-03-11 22:09:33 +0100

    Changed to canvas size and added a static player
    Canvas now uses the whole width and height of the browser window, these changes are made in the CSS.  Also added a static "player"  to the canvas with some attributes of the player's starting location and the width and height.

--- game/game.js
+++ game/game.js
@@ -1,50 +1,70 @@
 var ctx, width, height, canvas;
 var Game = {};
-var Player = {};
+var Player = {};    
 var keys = {};
+var lastLoop = new Date;
 
-Game.width = $(window).width();
-Game.height = $(window).height();
+
+Game.width = window.innerWidth;
+Game.height = window.innerHeight;
 Game.fps = 60;
 Game.maxFrameSkip = 10;
 Game.skipTicks = 1000 / Game.fps;
 
+Player.x = Game.width / 10;
+Player.y = Game.height- (Game.height/2);
+Player.width = Game.width / 13;
+Player.height = Game.height / 2.5;
 
 /* -------------------------------------------------------------------------- */
+document.addEventListener("keydown", function (e) {
+        keys[e.keyCode] = true;
+});
 
+document.addEventListener("keyup", function (e) {
+        keys[e.keyCode] = false;
+});
+
+/* -------------------------------------------------------------------------- */
+Game.drawGFX = function() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(Player.x, Player.y, Player.width, Player.height);
+};
+
+/* -------------------------------------------------------------------------- */
 Game.initialize = function() {
     canvas = document.getElementById("gamecanvas");
     if (canvas && canvas.getContext) {
         ctx = canvas.getContext("2d");
         canvas.width = Game.width;
         canvas.height = Game.height;
+        Game.drawGFX();
     }
 };
 
 /* -------------------------------------------------------------------------- */
-
 Game.update = function(tick) {
-    Game.tick = tick;
+    Game.tick = tick; 
+    Game.input();
 }
 
 /* -------------------------------------------------------------------------- */
-
-Game.draw = function() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fill();
-};
+Game.input = function() {
+}
 
 /* -------------------------------------------------------------------------- */
 Game.pause = function() {
    this.paused = (this.paused) ? false : true;
 };
 
-
+var lastLoop = new Date;
 Game.run = (function() {
     var loops = 0;
     var nextGameTick = (new Date).getTime();
     var startTime = (new Date).getTime();
+    
     return function() {
         loops = 0;
         while (!Game.paused && (new Date).getTime() > nextGameTick && loops < Game.maxFrameSkip) {
@@ -52,12 +72,10 @@ Game.run = (function() {
             nextGameTick += Game.skipTicks;
             loops++;
         }
-        Game.draw();
     };
 })();
 
 /* -------------------------------------------------------------------------- */
-
 (function() {
     var onEachFrame;
     if (window.requestAnimationFrame) {
