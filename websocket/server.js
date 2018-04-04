c8d6647eb6975031b6a05e11cb9918919632cff3
a15osmla <osmarolandaverde@gmail.com>
2018-04-04 22:48:39 +0200

    Changed the server config file. Added functions for creating a new socket and sending messages

--- websocket/server.js
+++ websocket/server.js
@@ -1,30 +1,26 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require("socket.io").listen(server);
+players = [];
+connections = [];
 
-var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
+server.listen(process.env.PORT || 1337);
+console.log('server running')
+app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/index.html'); 
 });
-server.listen(1337, function() { });
 
-// create the server
-wsServer = new WebSocketServer({
-  httpServer: server
-});
-
-// WebSocket server
-wsServer.on('request', function(request) {
-  var connection = request.accept(null, request.origin);
-
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      // process WebSocket message
-    }
-  });
-
-  connection.on('close', function(connection) {
-    // close user connection
-  });
-});
+io.sockets.on('connection', function(socket) {
+    connections.push(socket);
+    console.log('Connected: %s connected', connections.length);
+    
+    // DC
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Disconnected: %s sockets connected", connections.length);
+    
+    // Send message 
+    socket.on("send message", function(data){
+        io.sockets.emit('new message', {msg: data});
+    });
+})
