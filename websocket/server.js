46e6803e6dc1dc4da517ce824efae8460ea69d8d
a15osmla <osmarolandaverde@gmail.com>
2018-04-08 21:05:29 +0200

    Players should now be assigned the right objects
    Fixed issues with the clients not being assigned the right player objects (first client gets assigned to p1, second to p2);

--- websocket/server.js
+++ websocket/server.js
@@ -45,14 +45,14 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function(socket) {
     connections.push(socket);
     console.log('Connected: %s connected', connections.length);
-    console.log(players.length);
     
     // Add a new player object for every connection (Player) 
     if(players.length % 2 == 0) {
-        players.push(new Player(100,100,100,100,5, socket.id, "p1"));  
+        players.push(new Player(100,100,100,100,5, socket.id, "p1")); 
     } else {
-        players.push(new Player(900,900,100,100,5, socket.id), "p2");   
+        players.push(new Player(900,900,100,100,5, socket.id, "p2"));  
     }
+
 
     // Send player object to client
     for(var x = 0; x < players.length; x++) {
@@ -61,16 +61,13 @@ io.sockets.on('connection', function(socket) {
             io.sockets.connected[socket.id].emit('player', players[x]);
         }   
     }
-   
-    
-
 //------------------------------------------------------------ Disconnect -----------------------------------------------------------------
     
     //DC
     socket.on('disconnect', function() {
         connections.splice(connections.indexOf(socket), 1);
-        players.splice(players.indexOf(socket), 1);
-        console.log("P.length: " + players.length);
+        players.pop();
+        console.log(players.length);
         console.log("A socket disconnected: %s sockets connected", connections.length);
     });
     
